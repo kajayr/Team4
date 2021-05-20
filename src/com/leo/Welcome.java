0ca1ad98da656e7bb4a594c49a6df74865a49fe8
Ronnin <rcollins779x@gmail.com>
2021-05-20 12:57:43 -0400

    Completed Secure Login

--- src/com/leo/Welcome.java
+++ src/com/leo/Welcome.java
@@ -11,21 +11,20 @@ public class Welcome {
     Login login = new Login();
 
     public void welcomeMessage(){
+
         System.out.println("Welcome to Bank NotAScam");
         System.out.println("This is Team4, your virtual assistant. How can I help you today?");
         System.out.println("Press a number in the list to continue the process");
-        System.out.println("1- Setup a new account");
-        System.out.println("2- Login");
-        int input = SztChoice();
-        if(input == 1) {
-            openAccount();
+        for (int index = 0; index == 0;) {
+            System.out.println("1- Setup a new account");
+            System.out.println("2- Login");
+            int input = SztChoice();
+            if(input == 1) index = openAccount();
+            if(input == 2) index = login.secureLogin();
         }
-        if(input == 2){
-            //login.secureLogin();
-            //login.greetUser(customer);
-        }
+
     }
-    public void openAccount() {
+    public int openAccount() {
         System.out.println("Thank you for choosing us. Please select an option: ");
         System.out.println("First We need some information from you in order to create your account");
         User customer = new User();
@@ -56,10 +55,14 @@ public class Welcome {
         System.out.println("Please enter your email address so we can sell it to car warranty companies: ");
         customer.setEmail(EmailChoice());
         try {
-            appendNewUserToLoginRecords(customer);
-            appendNewUserToCheckingRecords(customer);
+            if( appendNewUserToLoginRecords(customer) != appendNewUserToCheckingRecords(customer)) {
+                System.out.println("Database Mismatch Detected. Launch all nuclear missiles!");
+                return 0;
+            }
         } catch (IOException e) {
             System.out.println("We cannot open an account at this time either because your internet sucks or Cathy crashed the server again.");
+            return 0;
         }
+        return customer.getIndex();
     }
 }
